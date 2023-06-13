5975c8bf2b5b2fb968e8320856e65b361309097e
Paula Rusti <rusti.paula78@gmail.com>
2023-06-13 11:49:38 +0300

    add swagger docs

---
+++ api/app.js
@@ -0,0 +1,11 @@
+const app = require('express')()
+const http = require('http')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('../swagger_output.json')
+
+http.createServer(app).listen(3000)
+console.log("Listening at:// port:%s (HTTP)", 3000)
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+require('./endpoints')(app)
