c20dbafefb5b0980d593650670e288e86354c167
Paula Rusti <rusti.paula78@gmail.com>
2023-06-13 12:09:55 +0300

    tried to encapsulate in a class but does not work for the moment

---
+++ api/ExpressAPI.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+class ExpressAPI {
+    constructor() {
+        this.app = express();
+        this.port = 3000; // default port
+        this.setupMiddlewares();
+        this.setupRoutes();
+    }
+
+    setupMiddlewares() {
+        // Add any required middlewares here
+        this.app.use(express.json()); // example: JSON body parser
+        // You can add more middlewares as per your needs
+    }
+
+    setupRoutes() {
+        // Example route with parameters
+        const routes = require('./endpoints');
+        this.app.use('/users', routes);
+    }
+
+    start() {
+        this.setupRoutes();
+        this.app.listen(this.port, () => {
+            console.log(`ExpressAPI is running on port ${this.port}`);
+        });
+    }
+}
+
+// Usage:
+const api = new ExpressAPI();
+api.start();
--- api/endpoints.js
+++ api/endpoints.js
@@ -1,8 +1,17 @@
 module.exports = function (app) {
 
-    app.get('/users/:id', (req, res) => {
+    app.get('/:id', (req, res) => {
 
         const filtro = req.query.filtro
+
+        return res.status(404).send(false)
+
+    })
+
+    app.post('/crawler', (req, res) => {
+
+        console.log("Received request to create crawler job")
+        const body = req.body
 
         return res.status(404).send(false)
 
